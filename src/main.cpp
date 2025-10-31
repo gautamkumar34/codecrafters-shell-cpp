38db212d21f2d652ae3259812089eba2e5528ac3
gautamkumar34 <142677394+gautamkumar34@users.noreply.github.com>
2025-10-31 12:45:02 +0530

    invalid command

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  cout << std::unitbuf;
+  cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  cout << "$ ";
+  string s;
+  getline(cin,s);
+  cout<<s<<": command not found"<<endl;
 }
