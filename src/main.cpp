d76af13ffa8b50e8a6190035e783ad20fdc4a594
gautamkumar34 <142677394+gautamkumar34@users.noreply.github.com>
2026-01-28 09:21:16 +0530

    implemented locate executable files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
+#include <experimental/filesystem>
+#include <cstdlib>   // for getenv
+#include <unistd.h>  // for access()
+
 using namespace std;
+namespace fs = std::experimental::filesystem;
+
+string get_path(string cmd) {
+    const char* path_env = getenv("PATH");
+    if(path_env==nullptr){
+      cout<<cmd<<": not found"<<endl;
+      return;
+    }
+
+    //split by delimiter
+    string path_str(path_env);
+    stringstream ss(path_str);
+    string directory ;
+
+    while (getline(ss, directory, ':')) {
+        if(directory.empty())continue;
+
+        fs::path full_path = fs::path(directory) /cmd;
+        if(fs::exists(full_path) && !fs::is_directory(full_path)){
+          if(access(full_path.c_str(),X_OK)==0){
+            cout<<cmd<<" is "<<full_path.string()<<endl;
+            return;
+          }
+        }
+    }
+
+    cout<<cmd<<": not found"<<endl;
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
