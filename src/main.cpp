1b5b64dd763f3a65d865d814ea4fc2838dd3f6b4
gautamkumar34 <142677394+gautamkumar34@users.noreply.github.com>
2026-03-29 21:34:35 +0530

    bug fix

--- src/main.cpp
+++ src/main.cpp
@@ -142,12 +142,28 @@ vector<string> file_completion(const string& prefix ){
       string name = ep->d_name;
       if(name == "." || name == "..")continue;
       if(name.find(file_prefix)==0){
-        string path_to_stat = (dir_path== "." ? name : dir_path +name);
-        struct stat statbuff;
-        if(stat(path_to_stat.c_str(), &statbuff)==0){
-          if(S_ISDIR(statbuff.st_mode)){
-            name += "/";
-          }
+        bool is_dir = false;
+
+        if (ep->d_type == DT_DIR) {
+            is_dir = true;
+        } 
+        else if (ep->d_type == DT_UNKNOWN || ep->d_type == DT_LNK) {
+            string path_to_stat = dir_path;
+            if (path_to_stat != "./" && path_to_stat != "." && path_to_stat.back() != '/') {
+                path_to_stat += "/";
+            }
+            if (path_to_stat == ".") path_to_stat = "";
+            path_to_stat += name;
+            
+            struct stat statbuf;
+            if(stat(path_to_stat.c_str(), &statbuf) == 0){
+                if(S_ISDIR(statbuf.st_mode)){
+                    is_dir = true;
+                }
+            }
+        }
+        if(is_dir){
+          name += "/";
         }
         matches.insert(name);
       }
