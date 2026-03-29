7a4f4f2eb80328edd3fea662ba5b825c9c1ac839
gautamkumar34 <142677394+gautamkumar34@users.noreply.github.com>
2026-03-29 21:04:05 +0530

    bug fix

--- src/main.cpp
+++ src/main.cpp
@@ -209,7 +209,7 @@ string read_line_raw(){
       else if(matches.size()==1){
         string match = matches[0];
         string to_add = match.substr(prefix.length()) + " ";
-        if(is_cmd || match.back() != '/'){
+        if(match.back() != '/'){
           to_add += " ";
         }
         input += to_add;
